860bc9128133b2336f4b4dcf7f14e4739ca9689b
Tomas Camiña <caminiatomas@gmail.com>
2023-01-04 12:49:14 -0300

    Change div-buttons for buttons and style it

--- src/Components/PanelButton.jsx
+++ src/Components/PanelButton.jsx
@@ -4,24 +4,24 @@ export const PanelButton = () => {
     return(
         <>
             <div className='panelButton'>
-                <div className='button reset plus-size'>C</div>
-                <div className='button'><i class="fas fa-arrow-left"></i></div>
-                <div className='button plus-size'>&divide;</div>
-                <div className='button'>7</div>
-                <div className='button'>8</div>
-                <div className='button'>9</div>
-                <div className='button'>&times;</div>
-                <div className='button'>4</div>
-                <div className='button'>5</div>
-                <div className='button'>6</div>
-                <div className='button plus-size'>-</div>
-                <div className='button'>3</div>
-                <div className='button'>2</div>
-                <div className='button'>1</div>
-                <div className='button'>+</div>
-                <div className='button igual plus-size'>=</div>
-                <div className='button'>0</div>
-                <div className='button plus-size'>.</div>
+                <button className='button reset plus-size'>C</button>
+                <button className='button'><i class="fas fa-arrow-left"></i></button>
+                <button className='button plus-size'>&divide;</button>
+                <button className='button'>7</button>
+                <button className='button'>8</button>
+                <button className='button'>9</button>
+                <button className='button'>&times;</button>
+                <button className='button'>4</button>
+                <button className='button'>5</button>
+                <button className='button'>6</button>
+                <button className='button plus-size'>-</button>
+                <button className='button'>3</button>
+                <button className='button'>2</button>
+                <button className='button'>1</button>
+                <button className='button'>+</button>
+                <button className='button igual plus-size'>=</button>
+                <button className='button'>0</button>
+                <button className='button plus-size'>.</button>
             </div>
         </>
     )
