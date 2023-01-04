8bddb272c42eaf0441dfaaf4e9742d9fa9fcb0fd
Tomas Camiña <caminiatomas@gmail.com>
2023-01-03 23:34:14 -0300

    I deleted App.jsx because It was obsolet

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './Components/App'
+
+//CSS
+import './Assets/CSS/style.css'
+
+//EXPORTACIONES DE COMPONENTES
+import { Display } from './Components/Display'
+import { PanelButton } from './Components/PanelButton'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+    {/*Creamos el contenedor de la calculadora*/}
+    <div className='calculator-container'>.  
+
+      <Display />
+      <PanelButton />
+
+    </div>
+
   </React.StrictMode>,
 );
+
