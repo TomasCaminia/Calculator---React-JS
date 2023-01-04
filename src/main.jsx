54b658ce18bd25ab46a079c4c350883c1ea7111e
Tomas Camiña <caminiatomas@gmail.com>
2023-01-04 10:32:06 -0300

    Repaired errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-//CSS
-import './Assets/CSS/style.css'
-
-//EXPORTACIONES DE COMPONENTES
-import { Display } from './Components/Display'
-import { PanelButton } from './Components/PanelButton'
+import { App } from './Components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-    {/*Creamos el contenedor de la calculadora*/}
-    <div className='calculator-container'>.  
-
-      <Display/>
-      <PanelButton/>
-
-    </div>
-
+      <App />
+      
   </React.StrictMode>,
 );
 
